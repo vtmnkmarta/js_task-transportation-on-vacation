f8185d2840828638216803f9de2729cc1d94ccc8
Marta Vatamaniuk <vatamanukmarta@gmail.com>
2025-09-10 09:32:38 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,19 +5,19 @@
  */
 function calculateRentalCost(days) {
   const DAILY_RATE = 40;
-  const LONG_TERM = 7;
-  const SHORT_TERM = 3;
+  const LONG_TERM_DAYS = 7;
+  const SHORT_TERM_DAYS = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
   let rentalCost = days * DAILY_RATE;
 
-  if (days >= LONG_TERM) {
+  if (days >= LONG_TERM_DAYS) {
     rentalCost -= LONG_TERM_DISCOUNT;
 
     return rentalCost;
   }
 
-  if (days >= SHORT_TERM) {
+  if (days >= SHORT_TERM_DAYS) {
     rentalCost -= SHORT_TERM_DISCOUNT;
   }
 
